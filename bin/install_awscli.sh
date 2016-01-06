b3b923375d86bb7524efe14efa5ab747505eccbd
Oded Lazar <odedlaz@gmail.com>
2016-01-06 10:57:48 +0200

    generating credentials file on startup

--- bin/install_awscli.sh
+++ bin/install_awscli.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
+INSTALL_DIR="/app/vendor/awscli"
 chmod +x /app/vendor/awscli-bundle/install
-/app/vendor/awscli-bundle/install -i /app/vendor/awscli
-chmod u+x /app/vendor/awscli/bin/aws
+/app/vendor/awscli-bundle/install -i $INSTALL_DIR
+chmod u+x $INSTALL_DIR/bin/aws
+
+mkdir ~/.aws
+
+cat >> ~/.aws/credentials << EOF
+[default]
+aws_access_key_id = $AWS_KEY
+aws_secret_access_key = $AWS_SECRET_KEY
+EOF
+
+cat >> ~/.aws/config << EOF 
+[default]
+region = $AWS_REGION
+EOF
